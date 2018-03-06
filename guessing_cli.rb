b9282c5037b6e99bb024cbee5bae37134d981cef
Celeste Gerard <celeste.gerard@gmail.com>
2018-03-06 18:58:13 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,11 @@
-# Code your solution here!
+def run_guessing_game(user_input)
+  WHILE LOOP
+  RANDOM
+  random_number = rand(0)
+  BREAK
+  if user_input == (rand(x))
+  puts "You guessed the correct number!"
+else
+  puts "The computer guessed "
+  
+end
